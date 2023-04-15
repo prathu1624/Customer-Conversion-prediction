150a2c981dcabe4d17caf196280fdf124df4e7d1
Prathamesh Krishna Kadam <119112838+prathu1624@users.noreply.github.com>
2023-04-15 11:56:25 +0530

    Update customer_conversion_pk.py

--- customer_conversion_pk.py
+++ customer_conversion_pk.py
@@ -1,3 +1,5 @@
+#Customer conversion prediction : - GUVI data science project by Prathamesh Kadam
+
 #importing the required packages
 import pandas as pd
 import numpy as np
@@ -5,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import metrics # evaluation metrics
 
-dataset = pd.read_csv(#"dataset_link") #reading the dataset
+dataset = pd.read_csv(#CSV file_path) #reading the dataset
 
 print(dataset.shape)
 dataset.head()
@@ -29,6 +31,8 @@ print('Class 1:', target_count[1])
 print('Proportion of class 0 is ', round(target_count[0] * 100 / (target_count[1] + target_count[0]), 2),'%')
 
 target_count.plot(kind='bar', title='Count (y)');
+
+"""Removing Outliers"""
 
 #age column
 iqr = dataset.age.quantile(0.75) - dataset.age.quantile(0.25)
@@ -69,6 +73,14 @@ for column in cat_vr:
   plt.ylabel("No. of customers")
   plt.title(column)
 
+#age column
+plt.figure(figsize=(30,10))
+sns.countplot(data = dataset, x='age', hue="y")
+
+#num_calls column
+plt.figure(figsize=(20,10))
+sns.countplot(data = dataset, x='num_calls', hue="y")
+
 #Replacing unknown values in education_qual and job columns with mode
 dataset = dataset.copy()
 dataset['education_qual'] = dataset['education_qual'].replace('unknown', dataset['education_qual'].mode()[0]) #imputing education_qual unknown
@@ -81,8 +93,6 @@ dataset['prev_outcome'] = dataset['prev_outcome'].replace('unknown', 'unknown_pr
 dataset.columns
 
 """#Encoding of categorical variables"""
-
-#cat_vr[:-1]
 
 #encoding categorical variables
 
@@ -167,7 +177,7 @@ from sklearn.metrics import roc_auc_score
 #f1_score(y_test,y_pred)
 y_pred = logistic_regression.predict_proba(x_test)
 auroc = roc_auc_score(y_test,y_pred[:,1])
-auroc
+print("Test set auc: {:.2f}".format(auroc))
 
 #Evaluating Decisiontreeclassifier
 y_pred = dt.predict(x_test) # Model's predictions
@@ -177,7 +187,7 @@ print("Test set auc: {:.2f}".format(acc))
 #Evaluating KNeighbours Classifier
 y_pred = knn.predict(x_test)
 auroc = roc_auc_score(y_test,y_pred)
-auroc
+print("Test set auc: {:.2f}".format(auroc))
 
 """#Ensembling"""
 
@@ -190,11 +200,46 @@ model3 = KNeighborsClassifier(3)
 model = VotingClassifier(estimators=[('lr', model1), ('dt', model2),('knn',model3)], voting='soft')
 model.fit(x_train,y_train)
 pred = model.predict(x_test)
-roc_auc_score(y_test,model.predict_proba(x_test)[:,1])
+auroc = roc_auc_score(y_test,model.predict_proba(x_test)[:,1])
+print("Test set auc: {:.2f}".format(auroc))
 
 #bagging classifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn import tree
 model = BaggingClassifier(tree.DecisionTreeClassifier(max_depth = 1, random_state=1), n_estimators=100)
 model.fit(x_train, y_train)
-roc_auc_score(y_test,model.predict_proba(x_test)[:,1])
+auroc = roc_auc_score(y_test,model.predict_proba(x_test)[:,1])
+print("Test set auc: {:.2f}".format(auroc))
+
+#RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+rf = RandomForestClassifier(n_estimators= 100, max_depth = 3, max_features='sqrt')
+rf.fit(x_train,y_train)
+pred = rf.predict(x_test)
+auroc = roc_auc_score(y_test,rf.predict_proba(x_test)[:,1])
+print("Test set auc: {:.2f}".format(auroc))
+
+#XGBOOST Classifier
+import xgboost as xgb
+from sklearn.model_selection import cross_val_score
+import numpy as np
+for lr in [0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.11,0.12,0.13,0.14,0.15,0.2,0.5,0.7,1]:
+  model = xgb.XGBClassifier(learning_rate = lr, n_estimators=100, verbosity = 0) # initialise the model
+  model.fit(x_train,y_train) #train the model
+  model.score(x_test, y_test) # scoring the model - r2 squared
+  print("Learning rate : ", lr, " Train score : ", model.score(x_train,y_train), " Cross-Val score : ", np.mean(cross_val_score(model, x_train, y_train, cv=10)))
+
+model = xgb.XGBClassifier(learning_rate = 0.04, n_estimators=100)
+model.fit(x_train,y_train) #train the model
+model.score(x_test, y_test) # scoring the model - r2 squared
+
+"""#Important Feature"""
+
+importances = rf.feature_importances_
+sorted_indices = np.argsort(importances)[::-1]
+feat_labels = dataset.columns[1:]
+
+for f in range(x_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30,
+                    feat_labels[sorted_indices[f]],
+                    importances[sorted_indices[f]]))
